a4399daab3e5dfd224e099f0edb2f438561be1f4
Aurélien Euillot <a.euillot@mavillemonshopping.fr>
2021-02-09 15:18:58 +0100

    add permited hosts config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['CORS_ORIGINS'].split(',').map { |origin| origin.strip }
+    origins '*'
 
     resource '*',
       headers: :any,
