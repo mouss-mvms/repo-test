a73faed4bd9221945ab874278d7f59db470432cf
Aurélien Euillot <a.euillot@mavillemonshopping.fr>
2021-02-11 16:31:32 +0100

    add rswag ui route and swagger schemas

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -27,6 +27,208 @@ RSpec.configure do |config|
           url: ENV['BASE_URL'].to_s
         }
       ]
+    },
+    'v1/swagger.yaml' => {
+      openapi: '3.0.1',
+      info: {
+        title: 'API V1',
+        version: 'v2'
+      },
+      paths: {},
+      servers: [
+        {
+          url: ENV['BASE_URL'].to_s
+        }
+      ],
+      components: {
+        schemas: {
+          Shop: {
+            type: 'object',
+            properties: {
+              name: {
+                type: 'string',
+                exemple: 'Jardin Local',
+                description: 'Display name of a shop.'
+              },
+              slug: {
+                type: 'string',
+                example: 'shops/jardin-local',
+                pattern: 'shops\/[a-z0-9-]+',
+                description: 'Resource Name of a shop.'
+              },
+              products: {
+                type: 'array',
+                items: {
+                  '$ref': '#/components/schemas/Product'
+                },
+                description: 'List of products.',
+              }
+            }
+          },
+          Product: {
+            type: 'object',
+            properties: {
+              name: {
+                type: 'string',
+                exemple: 'Chaise longue',
+                description: 'Display name of a product.'
+              },
+              slug: {
+                type: 'string',
+                example: 'products/chaise-longue',
+                pattern: 'products\/[a-z0-9-]+',
+                description: 'Resource Name of a product.'
+              },
+              description: {
+                type: 'string',
+                description: 'Description of a product.'
+              },
+              category: {
+                '$ref': '#/components/schemas/Category',
+                description: 'Category of a product.'
+              },
+              brand: {
+                '$ref': '#/components/schemas/Brand',
+                description: 'Brand of a product.'
+              },
+              status: {
+                type: 'string',
+                example: 'online',
+                default: 'not_online',
+                enum: ['not_online', 'online', 'draft_cityzen', 'submitted', 'refused'],
+                description: 'Status of a product.'
+              },
+              sellerAdvice: {
+                type: 'string',
+                description: 'Seller advice of a product'
+              },
+              isService: {
+                type: 'boolean',
+                description: 'This product is a merchandise or a service.'
+              },
+              variants: {
+                type: 'array',
+                items: {
+                  '$ref': '#/components/schemas/Variant'
+                },
+                description: 'List of variants.'
+              }
+            },
+            required: %w[name slug isService]
+          },
+          Category: {
+            type: 'object',
+            properties: {
+              slug: {
+                type: 'string',
+                example: 'categories/mobilier-exterieur',
+                pattern: 'categories\/[a-z0-9-]+',
+                description: 'Resource Name of a category.'
+              },
+              name: {
+                type: 'string',
+                example: 'Mobilier extérieur',
+                description: 'Display name of a category.'
+              }
+            }
+          },
+          Brand: {
+            type: 'object',
+            properties: {
+              name: {
+                type: 'string',
+                example: 'Magimix',
+                description: 'Display name of a brand.'
+              }
+            }
+          },
+          Variant: {
+            type: 'object',
+            properties: {
+              basePrice: {
+                type: 'number',
+                format: 'double',
+                example: 20.50,
+                description: 'Base price of a variant.'
+              },
+              weight: {
+                type: 'number',
+                format: 'double',
+                nullable: true,
+                example: 20.50,
+                description: 'Weight in grams of a variant.'
+              },
+              quantity: {
+                type: 'integer',
+                example: 20,
+                description: 'Quantity in stock of a variant.'
+              },
+              isDefault: {
+                type: 'boolean',
+                default: false,
+                description: 'Default state of a variant.'
+              },
+              goodDeal: {
+                '$ref': '#/components/schemas/GoodDeal',
+                description: 'Good deal of a variant.'
+              },
+              characteristics: {
+                type: 'array',
+                items: {
+                  '$ref': '#/components/schemas/Characteristic'
+                },
+                description: 'List of characteristics.'
+              }
+            },
+            required: %w[basePrice quantity]
+          },
+          Characteristic: {
+            type: 'object',
+            properties: {
+              name: {
+                type: 'string',
+                example: 'blue',
+                description: 'Display name of a characteristic.'
+              },
+              type: {
+                type: 'string',
+                example: 'color',
+                enum: ['color', 'size'],
+                description: 'Type of a characteristic.'
+              }
+            },
+            required: %w[name type]
+          },
+          GoodDeal: {
+            type: 'object',
+            properties: {
+              startsAt: {
+                type: 'string',
+                format: 'date',
+                example: '20/01/2021',
+                description: 'Start date of a good deal.'
+              },
+              endsAt: {
+                type: 'string',
+                format: 'date',
+                example: '16/02/2021',
+                description: 'End date of a good deal.'
+              },
+              discount: {
+                type: 'integer',
+                example: '20',
+                minimum: 1,
+                maximum: 100,
+                description: 'Discount amount of a good deal.'
+              }
+            },
+            required: %w[startAt endAt discount]
+          },
+          Order: {
+            type: 'object'
+          }
+        }
+      }
     }
   }
 
@@ -34,5 +236,5 @@ RSpec.configure do |config|
   # The swagger_docs configuration option has the filename including format in
   # the key, this may want to be changed to avoid putting yaml in json files.
   # Defaults to json. Accepts ':json' and ':yaml'.
-  config.swagger_format = :json
+  config.swagger_format = :yaml
 end
