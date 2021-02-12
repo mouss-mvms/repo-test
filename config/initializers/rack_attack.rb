381597fa0540b97988c722c09fbc5c261972eda0
Loic Boussiquet <l.boussiquet@mavillemonshopping.fr>
2021-02-12 16:22:25 +0100

    Ajout de la route api-docs en whitelist (normalement ...)

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -3,6 +3,10 @@ class Rack::Attack
     req.env["HTTP_APIKEY"] != ENV["HTTP_APIKEY"]
   end
 
+  safelist("allow access to api docs") do |req|
+    req.path.start("/api-docs")
+  end
+
   safelist("allow from localhost") do |req|
     "127.0.0.1" == req.ip || "::1" == req.ip
   end
