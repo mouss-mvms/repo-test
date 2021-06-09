9e1cd5fcba9a1f19c28d3463e84154c192fb7a5b
Romain Grossard <r.grossard@mavillemonshopping.fr>
2021-06-09 15:00:36 +0200

    Clean up Shrine initializer for API

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -8,39 +8,14 @@ s3_options = {
   secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
 }
 
-old_s3_options = {
-  bucket: "e-city",
-  region: "eu-central-1",
-  access_key_id: "AKIAIDWT2IFATUZXDWCA",
-  secret_access_key: "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"
-}
-
 Shrine.storages = {
-  cache: Shrine::Storage::S3.new(prefix: "cache", **s3_options),
-  store: Shrine::Storage::S3.new(public: true, **s3_options),
-  old_store: Shrine::Storage::S3.new(**old_s3_options)
+  store: Shrine::Storage::S3.new(public: true, **s3_options)
 }
 
-Shrine.plugin :activerecord
-Shrine.plugin :cached_attachment_data
-Shrine.plugin :restore_cached_data
-Shrine.plugin :data_uri
-Shrine.plugin :infer_extension, force: true
-Shrine.plugin :validation
 Shrine.plugin :validation_helpers
 Shrine.plugin :pretty_location
-Shrine.plugin :presign_endpoint, presign_options: -> (request) {
-                          filename = request.params["filename"]
-                          type = request.params["type"]
-                          {
-                            content_disposition: "inline; filename=\"#{filename}\"",
-                            content_type: type,
-                            content_length_range: 1..(10*1024*1024),
-                          }
-                        }
 Shrine.plugin :derivatives
 Shrine.plugin :model, cache: false
-Shrine.plugin :url_options, old_store: -> (io, **) { { public: true } }
 Shrine.plugin :add_metadata
 Shrine.plugin :default_storage, store: :store
 Shrine.plugin :remote_url, max_size: 20*1024*1024
