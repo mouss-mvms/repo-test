5e3b33b36991348def34e2bb213f7ab7b9664204
Romain Grossard <r.grossard@mavillemonshopping.fr>
2021-07-28 14:25:36 +0200

    Update swagger

--- spec/swagger/requests/api/citizens/products/products_spec.rb
+++ spec/swagger/requests/api/citizens/products/products_spec.rb
@@ -3,6 +3,23 @@ require 'swagger_helper'
 RSpec.describe 'api/citizens/products', type: :request do
   path '/api/citizens/{id}/products' do
     parameter name: 'id', in: :path, type: :string, description: 'Unique identifier of the citizen.'
+    parameter name: :city, in: :query, type: :string, description: 'City or Territory slug', example: 'bordeaux'
+    parameter name: :category, in: :query, type: :string, description: 'Category slug', example: "vin-et-spiritueux/aperitif-et-spiritueux/rhum"
+    parameter name: :perimeter, in: :query, schema:{
+      type: :string,
+      enum: ["none", "around_me", "all"],
+      description: 'Search perimeter'
+    }
+    parameter name: :price, in: :query, type: :string, description: 'Prices range', example: '4__19'
+    parameter name: :services, in: :query,type: :string, example: "livraison-par-la-poste__click-collect", description: 'Service slugs concatened with double "_" if more than one. (livraison-par-la-poste, livraison-france-metropolitaine, livraison-de-proximite, click-collect, e-reservation, livraison-par-colissimo, livraison-express-par-stuart, livraison-par-le-commercant, retrait-drive)'
+    parameter name: :sort_by, in: :query, schema: {
+      type: :string,
+      enum: [
+        "price-asc",
+        "price-desc",
+        "newest"
+      ]
+    }
 
     get('retrieve products of a citizen') do
       tags 'Citizens'
--- spec/swagger/requests/api/shops/products/products_spec.rb
+++ spec/swagger/requests/api/shops/products/products_spec.rb
@@ -5,6 +5,23 @@ RSpec.describe 'api/shops/products', type: :request do
   path '/api/shops/{shop_id}/products' do
     # You'll want to customize the parameter types...
     parameter name: 'shop_id', in: :path, type: :string, description: 'Unique identifier of the desired shop.'
+    parameter name: :city, in: :query, type: :string, description: 'City or Territory slug', example: 'bordeaux'
+    parameter name: :category, in: :query, type: :string, description: 'Category slug', example: "vin-et-spiritueux/aperitif-et-spiritueux/rhum"
+    parameter name: :perimeter, in: :query, schema:{
+      type: :string,
+      enum: ["none", "around_me", "all"],
+      description: 'Search perimeter'
+    }
+    parameter name: :price, in: :query, type: :string, description: 'Prices range', example: '4__19'
+    parameter name: :services, in: :query,type: :string, example: "livraison-par-la-poste__click-collect", description: 'Service slugs concatened with double "_" if more than one. (livraison-par-la-poste, livraison-france-metropolitaine, livraison-de-proximite, click-collect, e-reservation, livraison-par-colissimo, livraison-express-par-stuart, livraison-par-le-commercant, retrait-drive)'
+    parameter name: :sort_by, in: :query, schema: {
+      type: :string,
+      enum: [
+        "price-asc",
+        "price-desc",
+        "newest"
+      ]
+    }
 
     get('list products') do
       tags 'Products'
