e931131b67c17810fe4d717def33bd1838208c00
Romain Grossard <r.grossard@mavillemonshopping.fr>
2021-10-11 11:34:02 +0200

    Add route

---
+++ app/controllers/api/v1/brands/summaries_controller.rb
@@ -0,0 +1,11 @@
+module Api
+  module V1
+    module Brands
+      class SummariesController < ApplicationController
+        def search
+
+        end
+      end
+    end
+  end
+end
--- config/routes/v1/brands.rb
+++ config/routes/v1/brands.rb
@@ -1,3 +1,9 @@
 scope :auth do
   resources :brands, only: [:create]
 end
+
+namespace :brands do
+  resources :summaries, only: [:index] do
+    post :search, on: :collection
+  end
+end
