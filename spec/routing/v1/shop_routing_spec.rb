ea12a319178e09a535524ef42f8022c983540b65
Romain Grossard <r.grossard@mavillemonshopping.fr>
2021-10-15 15:08:59 +0200

    remove routes

--- spec/routing/v1/shop_routing_spec.rb
+++ spec/routing/v1/shop_routing_spec.rb
@@ -6,6 +6,5 @@ RSpec.describe "routes for Shops", :type => :routing do
   it { should route(:put, '/api/v1/auth/shops/26').to(controller: 'api/v1/shops', action: :update, id: 26) }
   it { should route(:get, '/api/v1/shops').to(controller: 'api/v1/shops', action: :index)}
   it { should route(:get, '/api/v1/shops/26/deliveries').to(controller: 'api/v1/shops/deliveries', action: :index, id: 26)}
-  it { should route(:get, '/api/v1/shops/summaries').to(controller: 'api/v1/shops/summaries', action: :index)}
   it { should route(:post, '/api/v1/shops/summaries/search').to(controller: 'api/v1/shops/summaries', action: :search)}
 end
