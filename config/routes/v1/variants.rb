1c337710d6f95dc6bfdc71cca45c7af93dd711db
Leloup <t.leloup@mavillemonshopping.fr>
2021-11-09 09:25:45 +0100

    patch variant offline

--- config/routes/v1/variants.rb
+++ config/routes/v1/variants.rb
@@ -1,3 +1,5 @@
 scope :auth do
   patch 'variants/:id', to: 'variants#update'
 end
+
+patch 'variants/:id', to: 'variants#update_offline'
--- spec/controllers/api/v1/variants_controller_spec.rb
+++ spec/controllers/api/v1/variants_controller_spec.rb
@@ -46,7 +46,7 @@ RSpec.describe Api::V1::VariantsController, type: :controller do
           request.env["CONTENT_TYPE"] = "multipart/form-data"
           uploaded_file = fixture_file_upload(Rails.root.join("spec/fixtures/files/images/harry-and-marv.jpg"), 'image/jpeg')
 
-          patch :update, params: variant_params.merge(id:  reference.id, file: uploaded_file)
+          patch :update, params: variant_params.merge(id: reference.id, file: uploaded_file)
           should respond_with(200)
           result = JSON.parse(response.body, symbolize_names: true)
           expect(result[:basePrice]).to eq(variant_params[:basePrice])
@@ -105,6 +105,49 @@ RSpec.describe Api::V1::VariantsController, type: :controller do
           patch :update, params: variant_params.merge(id: reference.id)
           should respond_with(403)
         end
+      end
+    end
+  end
+
+  describe "PATCH #update_offline" do
+    context "All ok" do
+      it 'should return 200 HTTP status code with the updated variant' do
+        provider = create(:api_provider, name: 'wynd')
+        user_shop_employee = create(:shop_employee_user, email: "shop.employee3@ecity.fr")
+        api_provider_variant = ApiProviderVariant.create!(api_provider: provider, external_variant_id: "12vd1")
+        reference = create(:reference, api_provider_variant: api_provider_variant)
+        product = reference.product
+        user_shop_employee.shop_employee.shops << product.shop
+        user_shop_employee.shop_employee.save
+        request.env["CONTENT_TYPE"] = "multipart/form-data"
+        uploaded_file = fixture_file_upload(Rails.root.join("spec/fixtures/files/images/harry-and-marv.jpg"), 'image/jpeg')
+
+        patch :update_offline, params: variant_params.merge(id: reference.id, files: [uploaded_file], external_variant_id: "yh64b")
+
+        should respond_with(200)
+        result = JSON.parse(response.body, symbolize_names: true)
+        expect(result[:basePrice]).to eq(variant_params[:basePrice])
+        expect(result[:weight]).to eq(variant_params[:weight])
+        expect(result[:quantity]).to eq(variant_params[:quantity])
+        expect(result[:isDefault]).to eq(variant_params[:isDefault])
+        expect(result[:goodDeal]).to eq(JSON.parse(variant_params[:goodDeal], symbolize_names: true))
+        expect(result[:imageUrls]).to_not be_nil
+        hash_variant_params = JSON.parse(variant_params[:characteristics], symbolize_names: true)
+        variant_params_mapped = hash_variant_params.map { |c| [c[:value], c[:name]] }
+        result[:characteristics].each do |charac|
+          expect(variant_params_mapped.include?([charac[:name], charac[:type]])).to eq(true)
+        end
+      end
+    end
+
+    context "when it call without external_variant_id" do
+      it "should return HTTP status 422" do
+        reference = create(:reference)
+        request.env["CONTENT_TYPE"] = "multipart/form-data"
+        uploaded_file = fixture_file_upload(Rails.root.join("spec/fixtures/files/images/harry-and-marv.jpg"), 'image/jpeg')
+
+        patch :update_offline, params: variant_params.merge(id: reference.id, files: [uploaded_file], external_variant_id: "yh64b")
+        should respond_with(422)
       end
     end
   end
