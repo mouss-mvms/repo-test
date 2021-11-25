4597ec8ca3623c005dfc301da6d1202419bfdcd0
romain <r.grossard@mavillemonshopping.fr>
2021-11-25 10:38:52 +0100

    enhance conflicts resolution

--- spec/factories/selections.rb
+++ spec/factories/selections.rb
@@ -1,7 +1,10 @@
 FactoryBot.define do
   factory :selection do
-    name { 'selectionName' }
-    slug { 'selectionname' }
+    name { "voiture" }
+    description { "voiture true" }
+    begin_date { "2021-07-20 00:00:00" }
+    end_date { "2021-07-27 00:00:00" }
+    is_home { false }
     after(:create) do |selection|
       selection.tags << create(:tag)
     end
