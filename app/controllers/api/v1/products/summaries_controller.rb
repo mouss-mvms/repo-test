d94be7ce7ae00a37c7cee88ae7de733b845a51ea
SanchoDelNiglo <r.grossard@mavillemonshopping.fr>
2021-12-20 15:57:15 +0100

    review changes

--- app/controllers/api/v1/products/summaries_controller.rb
+++ app/controllers/api/v1/products/summaries_controller.rb
@@ -7,14 +7,14 @@ module Api
         RESULTS_PER_PAGE = 16
 
         def search
-          search_params = search_parameters
+          search_parameters = search_params
           search_criterias = ::Criterias::Composite.new(::Criterias::Products::Online)
                                                    .and(::Criterias::Products::NotInShopTemplate)
                                                    .and(::Criterias::NotInHolidays)
 
-          if search_params[:location]
-            territory = Territory.find_by(slug: search_params[:location])
-            city = City.find_by(slug: search_params[:location])
+          if search_parameters[:location]
+            territory = Territory.find_by(slug: search_parameters[:location])
+            city = City.find_by(slug: search_parameters[:location])
             if territory
               search_criterias.and(::Criterias::InTerritory.new(territory.slug))
               insee_codes = territory.insee_codes
@@ -24,9 +24,9 @@ module Api
             else
               raise ApplicationController::NotFound.new('Location not found')
             end
-            if search_params[:perimeter]
+            if search_parameters[:perimeter]
               search_criterias.remove(:insee_code, :territory_slug)
-              case search_params[:perimeter]
+              case search_parameters[:perimeter]
               when 'department'
                 search_criterias.and(::Criterias::CloseToYou.new(nil, insee_codes))
               when 'country'
@@ -37,27 +37,27 @@ module Api
             search_criterias.and(::Criterias::InCountry.new(nil))
           end
 
-          search_criterias.and(::Criterias::NotInCategories.new(Category.excluded_from_catalog.pluck(:id))) unless search_params[:q]
-          search_criterias.and(::Criterias::Products::SharedByCitizen) if search_params[:shared_products] == true
-          search_criterias.and(::Criterias::Products::FromServices.new(search_params[:services])) if search_params[:services]
+          search_criterias.and(::Criterias::NotInCategories.new(Category.excluded_from_catalog.pluck(:id))) unless search_parameters[:q]
+          search_criterias.and(::Criterias::Products::SharedByCitizen) if search_parameters[:shared_products] == true
+          search_criterias.and(::Criterias::Products::FromServices.new(search_parameters[:services])) if search_parameters[:services]
 
-          if search_params[:category]
-            category = Category.find_by!(slug: search_params[:category])
+          if search_parameters[:category]
+            category = Category.find_by!(slug: search_parameters[:category])
             search_criterias.and(::Criterias::InCategories.new([category.id]))
           end
 
-          if search_params[:selection_id]
-            selection = Selection.find(search_params[:selection_id])
+          if search_parameters[:selection_id]
+            selection = Selection.find(search_parameters[:selection_id])
             search_criterias.and(::Criterias::Products::FromSelection.new(selection))
           end
 
           search_results = ::Requests::ProductSearches.new(
-            query: search_params[:q],
+            query: search_parameters[:q],
             criterias: search_criterias.create,
-            sort_params: search_params[:sort_by],
-            random: search_params[:random],
+            sort_params: search_parameters[:sort_by],
+            random: search_parameters[:random],
             aggs: [:base_price, :brand_name, :colors, :sizes, :services, :category_tree_ids],
-            pagination: { page: search_params[:page], per_page: RESULTS_PER_PAGE }
+            pagination: { page: search_parameters[:page], per_page: RESULTS_PER_PAGE }
           ).call
 
           search = { products: search_results.map { |p| p }, aggs: search_results.aggs, page: search_results.options[:page], total_pages: search_results.total_pages }
@@ -71,23 +71,23 @@ module Api
 
         private
 
-        def search_parameters
-          search_parameters = {}
-          search_parameters[:location] = params[:location].parameterize if params[:location].present?
-          search_parameters[:perimeter] = params[:perimeter] if params[:perimeter]
-          search_parameters[:q] = params[:q] if params[:q].present?
-          search_parameters[:shared_products] = params[:sharedProducts] if params[:sharedProducts]
-          search_parameters[:category] = params[:category] if params[:category].present?
-          search_parameters[:selection_id] = params[:selectionId] if params[:selectionId].present?
-          search_parameters[:services] = params[:services] if params[:services]
-          search_parameters[:sort_by] = params[:sortBy] ? params[:sortBy] : 'name-asc'
-          search_parameters[:random] = params[:sortBy] == 'random'
-          search_parameters[:page] = params[:page] ? params[:page] : "1"
-          search_parameters
+        def search_params
+          search_params = {}
+          search_params[:location] = params[:location].parameterize if params[:location].present?
+          search_params[:perimeter] = params[:perimeter] if params[:perimeter]
+          search_params[:q] = params[:q] if params[:q].present?
+          search_params[:shared_products] = params[:sharedProducts] if params[:sharedProducts]
+          search_params[:category] = params[:category] if params[:category].present?
+          search_params[:selection_id] = params[:selectionId] if params[:selectionId].present?
+          search_params[:services] = params[:services] if params[:services]
+          search_params[:sort_by] = params[:sortBy] ? params[:sortBy] : 'name-asc'
+          search_params[:random] = params[:sortBy] == 'random'
+          search_params[:page] = params[:page] ? params[:page] : "1"
+          search_params
         end
 
         def cache_params
-          search_parameters
+          search_params
         end
       end
     end
