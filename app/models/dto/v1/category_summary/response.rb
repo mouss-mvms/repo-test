5ef8424e5dbbded396d00093eecf3100a24a4f10
Leloup <t.leloup@mavillemonshopping.fr>
2022-01-25 10:37:17 +0100

    use searchkick for categories routes

---
+++ app/models/dto/v1/category_summary/response.rb
@@ -0,0 +1,45 @@
+module Dto
+  module V1
+    module CategorySummary
+      class Response
+        attr_accessor :id, :name, :slug, :children, :has_children
+
+        def initialize(**args)
+          @id = args[:id]
+          @name = args[:name]
+          @slug = args[:slug]
+          @has_children = args[:has_children]
+          @children = args[:children]
+        end
+
+        def self.create(category, fields)
+          return nil if category.nil?
+          Dto::V1::CategorySummary::Response.new(
+            id: category.id,
+            name: category.name,
+            has_children: category.has_children,
+            slug: category.slug,
+            children: fields[:children] ? get_children(category) : []
+          )
+        end
+
+        def to_h(fields = nil)
+          hash = {}
+          hash[:id] = @id
+          hash[:name] = @name
+          hash[:slug] = @slug
+          hash[:hasChildren] = @has_children
+          hash[:children] = @children&.map { |child| child.to_h } if fields&.any? && fields.include?(:children)
+          hash
+        end
+
+        private
+
+        def self.get_children(category)
+          categories = ::Category.search("*", { where: { parent_id: category.id }, load: false })
+          categories.map { |child_category| self.create(child_category, { children: false }) }
+        end
+      end
+    end
+  end
+end
--- spec/swagger/v1/requests/api/categories_spec.rb
+++ spec/swagger/v1/requests/api/categories_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe 'api/v1/categories', swagger_doc: 'v1/swagger.json', type: :reque
 
   path '/api/v1/categories/roots' do
     parameter name: :children, in: :query, type: :boolean, description: 'Should return category children.'
-    parameter name: 'If-None-Match', in: :header, type: :string, description: 'Etag checker.'
 
     get('list parents categories') do
       tags 'Categories'
@@ -27,7 +26,6 @@ RSpec.describe 'api/v1/categories', swagger_doc: 'v1/swagger.json', type: :reque
   path '/api/v1/categories/{id}' do
     parameter name: :id, in: :path, type: :integer, description: 'Unique identifier of the category.', required: true
     parameter name: :children, in: :query, type: :boolean, description: 'Should return category children.'
-    parameter name: 'If-None-Match', in: :header, type: :string, description: 'Etag checker.'
 
     get('Show a category with it children') do
       tags 'Categories'
