a6a878b1256eb79b1d450041462e13c8f55b62ac
Leloup <t.leloup@mavillemonshopping.fr>
2022-01-31 10:20:23 +0100

    replace kaminari by pagy

--- app/controllers/api/v1/tags/products_controller.rb
+++ app/controllers/api/v1/tags/products_controller.rb
@@ -2,14 +2,14 @@ module Api
   module V1
     module Tags
       class ProductsController < ApplicationController
+        include Pagy::Backend
 
         DEFAULT_PER_PAGE = 15
 
         def index
-          page = params[:page].to_i || 1
           per_page = params[:limit] || DEFAULT_PER_PAGE
-          products = Kaminari.paginate_array(Tag.find(params[:id]).products.joins(:references).distinct.order(created_at: :desc).to_a).page(page).per(per_page)
-          response = { products: [], page: page, totalPages: products.total_pages }
+          pagination, products = pagy(Tag.find(params[:id]).products.joins(:references).distinct.order(created_at: :desc), items: per_page)
+          response = { products: [], page: pagination.page, totalPages: pagination.pages, totalCount: pagination.count }
           response[:products] = products.map { |product| Dto::V1::Product::Response.create(product).to_h }
           render json: response, status: :ok
         end
