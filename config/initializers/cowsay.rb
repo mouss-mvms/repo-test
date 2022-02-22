c2c9f9a9f032ffdfee02558d9456c79884c27c01
SanchoDelNiglo <r.grossard@mavillemonshopping.fr>
2022-02-22 11:19:55 +0100

    add env condition to cowsay

--- config/initializers/cowsay.rb
+++ config/initializers/cowsay.rb
@@ -1,12 +1,13 @@
-Cow::MAX_LINE_LENGTH = 65
-COWS = %w[bong udder mutilated small sodomized flaming-sheep head-in skeleton moose three-eyes vader satanic]
+if Rails.env.local? || Rails.env.test? || Rails.env.development?
+  Cow::MAX_LINE_LENGTH = 65
 
-def say(object)
-  cow = Cow.new(cow: COWS.sample, face_type: Cow.faces.sample)
-  (object.respond_to?(:inspect)) ? puts(cow.say(object.inspect)) : puts(cow.say(object))
-end
+  def say(object)
+    cow = Cow.new(cow: "bong", face_type: Cow.faces.sample)
+    (object.respond_to?(:inspect)) ? puts(cow.say(object.inspect)) : puts(cow.say(object))
+  end
 
-def think(object)
-  cow = Cow.new(cow: COWS.sample, face_type: Cow.faces.sample)
-  (object.respond_to?(:inspect)) ? puts(cow.think(object.inspect)) : puts(cow.think(object))
+  def think(object)
+    cow = Cow.new(cow: "bong", face_type: Cow.faces.sample)
+    (object.respond_to?(:inspect)) ? puts(cow.think(object.inspect)) : puts(cow.think(object))
+  end
 end
