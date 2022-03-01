32c812b3963df94ed0f02948b3e078ffcb1836ec
Leloup <t.leloup@mavillemonshopping.fr>
2022-03-01 10:38:38 +0100

    retour PR 2

--- app/controllers/api/v1/shops/products/images_controller.rb
+++ app/controllers/api/v1/shops/products/images_controller.rb
@@ -7,7 +7,7 @@ module Api
             shop = @user.shop_employee.shops.last
             raise ApplicationController::UnprocessableEntity.new unless shop
             product = shop.products.find(params[:product_id])
-            raise ApplicationController::Forbidden.new unless product.submitted? || product.online? || product.submitted?
+            raise ApplicationController::Forbidden.new unless product.submitted? || product.online? || product.offline?
             samples = product.samples
             image = Image.where(id: params[:id], sample_id: [samples.map(&:id)]).first
             raise ApplicationController::NotFound.new("Could not find Image with id: #{params[:id]} for product_id: #{params[:product_id]}") unless image
