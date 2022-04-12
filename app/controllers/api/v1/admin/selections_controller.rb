01286d5dbcd9d01c17ae4341ca3fcd7df39976c0
Leloup <t.leloup@mavillemonshopping.fr>
2022-04-12 14:39:50 +0200

    Change order to desc & fix pagination max

--- app/controllers/api/v1/admin/selections_controller.rb
+++ app/controllers/api/v1/admin/selections_controller.rb
@@ -6,7 +6,7 @@ module Api
           params[:page] ||= Pagy::DEFAULT[:page]
           params[:limit] ||= Pagy::DEFAULT[:items].to_i
           pagination, selections = pagy(Selection, items: params[:limit])
-          selection_responses = selections.order(id: :asc).map { |selection| Dto::V1::Selection::Response.create(selection).to_h }
+          selection_responses = selections.order(id: :desc).map { |selection| Dto::V1::Selection::Response.create(selection).to_h }
           response = { selections: selection_responses, page: pagination.page, totalCount: pagination.count, totalPages: pagination.pages }
           render json: response, status: :ok
         end
